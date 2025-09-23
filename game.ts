11aec1f11f3944d0c7386a1b669a0b2bf95ba9ff
Dmaury26 <151038515+Dmaury26@users.noreply.github.com>
2025-09-23 15:50:50 +0200

    Test de connexion avec git + implémentation de 2 fonctions permettant de générer une grille et vérifier si l'emplacement des bateaux est autorisé

---
+++ game.ts
@@ -0,0 +1,33 @@
+type Direction = 'horizontal' | 'vertical';
+type Cell = 'empty' | 'ship';
+type Grid = Cell[][];
+
+// 1. Créer une grille vide
+function createGrid(rows: number = 10, cols: number = 10): Grid {
+  const grid: Grid = [];
+
+  for (let i = 0; i < rows; i++) {
+    const row: Cell[] = [];
+    for (let j = 0; j < cols; j++) {
+      row.push('empty');
+    }
+    grid.push(row);
+  }
+  return grid;
+}
+
+// 2. Vérifier si le bateau peut être placé
+function canPlaceShip(grid: Grid, row: number, col: number, size: number, direction: Direction): boolean {
+  const rows = grid.length;
+  const cols = grid[0].length;
+
+  for (let i = 0; i < size; i++) {
+    const r = direction === 'vertical' ? row + i : row;
+    const c = direction === 'horizontal' ? col + i : col;
+
+    if (r >= rows || c >= cols || grid[r][c] !== 'empty') {
+      return false;
+    }
+  }
+  return true;
+}
